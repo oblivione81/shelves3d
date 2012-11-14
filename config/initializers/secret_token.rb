892fa2fc410cf225939fe6c73a3085a9556b0c17
oblivion <tessaro.daniele@gmail.com>
2012-11-14 21:52:52 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelves::Application.config.secret_token = 'e46c6254b5660b77b617ec8f4f45305ef031f214af2735a7233d35e74324d2fd74d2cd4d7cf0c1d62ad8de91aa1119065f6d9ed1b67b6e91d4dce4e7957a763e'

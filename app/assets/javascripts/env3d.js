68ebeef0029404d471513d11816150f56b48899f
oblivion <tessaro.daniele@gmail.com>
2012-11-16 12:43:03 +0100

    sdasd

--- app/assets/javascripts/env3d.js
+++ app/assets/javascripts/env3d.js
@@ -2,7 +2,7 @@
  * Created with JetBrains RubyMine.
  * User: oblivion
  * Date: 16/11/12
- * Time: 11.44
+ * Time: 12.38
  * To change this template use File | Settings | File Templates.
  */
 //= require three

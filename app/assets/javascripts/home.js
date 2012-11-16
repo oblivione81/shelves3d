7acee53bb4dc24c3ab1c1bdb17b4d69e3eb5bfea
oblivion <tessaro.daniele@gmail.com>
2012-11-16 12:46:34 +0100

    sdasd

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: oblivion
+ * Date: 16/11/12
+ * Time: 12.45
+ * To change this template use File | Settings | File Templates.
+ */
+//= require env3d
